11699790dd00e54db3c332b9f31b1afc6b8333b3
Eranmonnie <ajalaferanmi123@gmail.com>
2023-06-09 20:12:56 +0100

    server and socket setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const http = require('http');
+const socket = require('socket.io');
+const express = require('express');
+
+const app =  express();
+const server = http.createServer(app);
+const io = socket(server);
+const PORT = 3000  //|| process.env.PORT  if you have an env file 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+io.on('connection', socket=>{
+    console.log('connection set');
+})
+
+server.listen(PORT, ()=>{
+    console.log(`server started at port ${PORT}`);
+})
