1dfb2808fd4c7d6d577a086adea95ffc4110f073
Eranmonnie <ajalaferanmi123@gmail.com>
2023-06-10 09:13:53 +0100

    connection events

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const http = require('http');
-const socket = require('socket.io');
+const websocket = require('socket.io');
 const express = require('express');
 
 const app =  express();
 const server = http.createServer(app);
-const io = socket(server);
+const io = websocket(server);
 const PORT = 3000  //|| process.env.PORT  if you have an env file 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,8 +17,18 @@ app.get('/', (req, res)=>{
 })
 
 io.on('connection', socket =>{
-    console.log('connection set');
+   
+    //welcome current user
     socket.emit('message', 'welcome to my chat app');
+
+    //show that a user has joined the chat but not the one connecting
+    socket.broadcast.emit('message', 'a user has joined the chat');
+
+    //run to tell everyone that a user has disconnected
+
+    socket.on('disconnect', () =>{
+        io.emit('message', 'a user has disconnected');
+    })
 })
 
 server.listen(PORT, ()=>{
