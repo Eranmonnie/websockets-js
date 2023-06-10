06a0ec7ed7cc2b564c9613b2db43b8f9d3dd1bc5
Eranmonnie <ajalaferanmi123@gmail.com>
2023-06-10 11:19:39 +0100

    login page

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-    res.render('index');
+    res.render('login');
 })
 
 io.on('connection', socket =>{
