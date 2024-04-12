0c773066967be730e4c3945b9c901333fe12f5ed
Eranmonnie <ajalaferanmi123@gmail.com>
2024-04-12 02:22:35 +0100

    minor changes

--- app.js
+++ app.js
@@ -1,88 +1,93 @@
-const path = require('path');
-const http = require('http');
-const websocket = require('socket.io');
-const express = require('express');
-const formatmessage = require('./utils/messageformatter');
-const userstore = require('./utils/users');
+const path = require("path");
+const http = require("http");
+const websocket = require("socket.io");
+const express = require("express");
+const formatmessage = require("./utils/messageformatter");
+const userstore = require("./utils/users");
 
-const botname = 'Chat bot';
+const botname = "Chat bot";
 
-const app =  express();
+const app = express();
 const server = http.createServer(app);
 const io = websocket(server);
-const PORT = 3000  //|| process.env.PORT  if you have an env file 
+const PORT = 3000; //|| process.env.PORT  if you have an env file
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res)=>{
-    res.render('login');
-})
-
-app.get('/chat', (req, res)=>{
-    res.render('index');
-})
-
-io.on('connection', socket =>{
-
-     
-    socket.on('joinRoom', ({username, room}) =>{
-        //store user data in db
-        const user = userstore.UserJoin( socket.id, username, room);
-
-        //join a room 
-        socket.join(user.room);
-
-        //welcome current user
-        socket.emit('message', formatmessage.formatMessage(botname, `welcome ${user.username}`));
-        
-        //show that a user has joined the chat but not the one connecting
-        socket.broadcast.to(user.room).emit('message', formatmessage.formatMessage(botname, `${user.username} has joined the chat`) );
-
-        //retrieve users general data 
-        io.to(user.room).emit('roomUsers', {
-            room:user.room,
-            users: userstore.getRoomUsers(user.room),
-        });
-
-
-
-    });
-   
-   
-    //listen for client message 
-    socket.on('chatMessage', messasge =>{
-
-        const user = userstore.getCurrentUser(socket.id);
-
-        io.to(user.room).emit('message' ,formatmessage.formatMessage(user.username,  messasge));
- 
-    });
-
-
-     //run to tell everyone that a user has disconnected
-     socket.on('disconnect', () =>{
-
-        const user = userstore.userLeaves(socket.id);
-        
-        if (user){
-
-            io.to(user.room).emit('message' ,formatmessage.formatMessage(botname,  `${user.username} has left the chat`));
-
-            io.to(user.room).emit('roomUsers', {
-                room:user.room,
-                users: userstore.getRoomUsers(user.room),
-            });
-
-        }
-
-    })
-
-
-
+app.get("/", (req, res) => {
+  res.render("login");
 });
 
-server.listen(PORT, ()=>{
-    console.log(`server started at port ${PORT}: http://localhost:3000/`);
-})
+app.get("/chat", (req, res) => {
+  res.render("index");
+});
+
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    //store user data in db
+    const user = userstore.UserJoin(socket.id, username, room);
+
+    //join a room
+    socket.join(user.room);
+
+    //welcome current user
+    socket.emit(
+      "message",
+      formatmessage.formatMessage(botname, `welcome ${user.username}`)
+    );
+
+    //show that a user has joined the chat but not the one connecting
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatmessage.formatMessage(
+          botname,
+          `${user.username} has joined the chat`
+        )
+      );
+
+    //retrieve users general data
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: userstore.getRoomUsers(user.room),
+    });
+  });
+
+  //listen for client message
+  socket.on("chatMessage", (messasge) => {
+    const user = userstore.getCurrentUser(socket.id);
+
+    io.to(user.room).emit(
+      "message",
+      formatmessage.formatMessage(user.username, messasge)
+    );
+  });
+
+  //run to tell everyone that a user has disconnected
+  socket.on("disconnect", () => {
+    const user = userstore.userLeaves(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatmessage.formatMessage(
+          botname,
+          `${user.username} has left the chat`
+        )
+      );
+
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: userstore.getRoomUsers(user.room),
+      });
+    }
+  });
+  
+});
+
+server.listen(PORT, () => {
+  console.log(`server started at port ${PORT}: http://localhost:3000/`);
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,86 +1,87 @@
-const chatmessage = document.getElementById('message-box');
+const chatmessage = document.getElementById("message-box");
 
-
-// get user name and room from query string 
-const{username, room} = Qs.parse(location.search,
-    {
-        ignoreQueryPrefix:true
- });
-
-
-//socket connection 
-const socket = io();
-
- //log in to room
- socket.emit('joinRoom', 
- {
-  username,
-  room
- } );
-
-//  gat all users in rooms data 
-
- socket.on('roomUsers', ({room,users})=>{
-    
-    outputRoom(room);
-    outputUsers(users);
- });
- 
-socket.on('message', message =>{
-
-    addtodom(message);
-        
-   //scroll down 
-   chatmessage.scrollTop = chatmessage.scrollHeight;
-
-})
-
-const form = document.getElementById('form');
-
-form.addEventListener('submit', (e)=>{
-
-    e.preventDefault();
-
-    message = e.target.elements.msg.value;
-
-   //emit message to serve
-
-   socket.emit('chatMessage', message);
-
-   //clear form data 
-   e.target.elements.msg.value = '';
-   e.target.elements.msg.focus();
-   
+// get user name and room from query string
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
 });
 
+//socket connection
+const socket = io();
 
-function addtodom(message){
-    const div = document.createElement('div');
-    //add condition if its users message to add flex start to div
- 
-    div.innerHTML = `
+//log in to room
+socket.emit("joinRoom", {
+  username,
+  room,
+});
+
+//  gat all users in rooms data
+
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoom(room);
+  outputUsers(users);
+});
+
+socket.on("message", (message) => {
+  addtodom(message);
+
+  //scroll down
+  chatmessage.scrollTop = chatmessage.scrollHeight;
+});
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  message = e.target.elements.msg.value;
+
+  //emit message to serve
+
+  socket.emit("chatMessage", message);
+
+  //clear form data
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+function addtodom(message) {
+  const div = document.createElement("div");
+  //add condition if its users message to add flex start to div
+
+  div.innerHTML = `${
+    message.username == username
+      ? `
     <div class="flex justify-end mb-6 text-xs">
 
-    <div class='bg-gray-500 px-5 py-2 rounded-full rounded-tr-none'> 
-    <p class='mb-1'> ${message.username}</p>
-        <p class='mb-2'> ${message.message}</p>
-        <p class='w-full flex justify-end'> ${message.time}</p>
+    <div class='bg-[#007bff] p-3 rounded-full rounded-tr-none hover:bg-blue-700 transition text-white text-md'> 
+    <p class=''> ${message.message}</p>
+    <div class=' text-[10px] flex justify-between'>  <span>${message.username}</span>  <span>${message.time}</span> </div>  
     </div>
 
     </div>`
+      : `<div class="flex justify-start mb-6 text-xs">
 
-    document.getElementById('message-box').appendChild(div);
+    <div class='bg-[#007bff] p-3 rounded-full rounded-tl-none hover:bg-blue-700 transition text-white text-md'> 
+    <p class=''> ${message.message}</p>
+    <div class=' text-[10px] flex justify-between'>  <span>${message.username}</span>  <span>${message.time}</span> </div>  
+    </div>
+
+    </div>`
+  }`;
+
+  document.getElementById("message-box").appendChild(div);
 }
 
-
-function outputRoom(room){
-    const roomname = document.getElementById('roomname')
-    roomname.innerHTML = room;
-
+function outputRoom(room) {
+  const roomname = document.getElementById("roomname");
+  roomname.innerHTML = `<div class="text-white">${room}</div>`;
 }
-function outputUsers(users){
-    const room = document.getElementById('users')
-    
-    room.innerHTML = `${users.map(user=>`<li class='mt-2 p-2'>${user.username}</li>`).join('')}`
-    
+function outputUsers(users) {
+  const room = document.getElementById("users");
+
+  room.innerHTML = `${users
+    .map(
+      (user) => `<li class='mt-2 p-2 text-black text-xs'>${user.username}</li>`
+    )
+    .join("")}`;
 }
